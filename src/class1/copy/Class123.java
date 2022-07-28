dd749f0cbeb6c7a4f23574ae7eb34a3bea07cd29
Lenovo <Lenovo@DESKTOP-VLAU41A>
2022-07-28 13:27:00 +0530

    fourth commit

---
+++ src/class1/copy/Class123.java
@@ -0,0 +1,31 @@
+package class1.copy;
+
+public class Class123 {
+public static void main(String[] args) {
+	 //variable declaration
+	 
+	String sname;
+	int srollNum;
+	int totalmark;
+	int stotalmarkgain;
+	char sgrade;
+	float sper;
+	
+	//variable intialization
+	
+	sname="Sameer";
+	srollNum=18;
+	totalmark=500;
+	stotalmarkgain=250;
+	sgrade='B';
+	sper=50f;
+	
+	
+	 System.out.println("student name"+sname);
+	 System.out.println("srollNum"+srollNum);
+	 System.out.println(" totalmark"+totalmark);
+	 System.out.println("studentotalmarkgain"+stotalmarkgain);
+	 System.out.println("student grade"+sgrade);
+	 System.out.println("spercentage"+sper);
+}
+}
---
+++ src/class1/copy/test123.java
@@ -0,0 +1,36 @@
+package class1.copy;
+
+public class test123 {
+
+public static void main(String args[]) {
+	
+	boolean flag;
+	flag=true;
+	System.out.println("status of flag" +flag);
+	
+	byte range;
+	range=127;
+	System.out.println("byte range" +range);
+	
+	int number;
+	number=123;
+	System.out.println("number of days" +number);
+	
+	short memory;
+	memory=2;
+	System.out. println("memory size of short is" +memory);
+	
+	float marks;
+	marks=90.90f;
+	System.out.println("marks of tenth" +marks);
+	
+	double abc;
+	abc=23.56678990;
+	System.out.println("double status" +abc);
+	
+	char singh;
+	singh='A';
+	System.out.println("grade"+singh);
+	
+}
+}
